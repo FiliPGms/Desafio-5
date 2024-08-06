09bb3580f9ff780e7712eb7d4fb82ce3b530354d
FiliPGms <166773370+FiliPGms@users.noreply.github.com>
2024-08-05 22:00:01 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
 const mariadb = require("mariadb");
 
 const pool = mariadb.createPool({
-    host: "localhost",
-    user: "root",
-    password: "jjks",
-    database: "turismo_maranhao",
-    connectionLimit: 10,
-    acquireTimeout: 30000,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectionLimit: process.env.DB_CONNECTIONLIMIT,
+    acquireTimeout: process.env.DB_ACQUIRETIMEOUT,
     port: 3306,
 })
 
